381168f8ffb5c52e330ba6e10bc557fcc4e33102
FS12321 <user.911@t-online.de>
2021-06-24 13:24:28 +0200

    new folders, main

--- SuS_WS_Gruppe3/main.c
+++ SuS_WS_Gruppe3/main.c
@@ -1,40 +1,70 @@
+//Aufgabe 1
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/adc.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+
+// Praeprozessor-Makros
+#define BUFFER_SIZE 1000
+#define SAMPLERATE 44000
 
 
-// copied files from Aufgabe_3-1_Blinkregister_Vorlage.c
-#include<stdint.h>
+// Funktionen-Deklarationen
+void adcIntHandler(void);
+void setup(void);
 
-// Warteschleife
-void delay(void)
-{
-    uint32_t i=50000;
-    while(i) {i--;}
+// globale Variablen
+// hier die benÃ¶tigten globalen Variablen, wie den Ringbuffer einfuegen
+
+void main(void){ // nicht veraendern!! Bitte Code in adcIntHandler einfuegen
+    setup();
+    while(1){}
 }
-int main(void)
-{
-    uint32_t* address_RCGCGPIO;
-    uint32_t* address_GPIO_DIR;
-    uint32_t* address_GPIO_DATA;
-    uint32_t* address_GPIO_DEN;
-    // Adresse RCGCGPIO Register
-    address_RCGCGPIO = (uint32_t*) 0x400FE608;
-    // Adresse DATA Register mit Bitmaskierung für Pin1 (LED_R), Pin2 (LED_B) und Pin3 (LED_G)
-    address_GPIO_DATA = (uint32_t*) 0x40025038;
-    // Adresse DEN Register
-    address_GPIO_DEN = (uint32_t*) 0x4002551C;
-    // Adresse DIR Register
-    address_GPIO_DIR = (uint32_t*) 0x40025400;
-    // Peripherie Port F aktivieren
-    *address_RCGCGPIO = *address_RCGCGPIO | (0x20);
-    // setze Pin als Ausgang
-    *address_GPIO_DIR = *address_GPIO_DIR| (0x0E);
-    // setze Pin als Digital
-    *address_GPIO_DEN = *address_GPIO_DEN| (0x0E);
-    while(1) {
-        // Setze Pin auf High Pegel
-        *address_GPIO_DATA =  (0x04);
-        delay();
-        // Setze Pin auf Low Pegel
-        *address_GPIO_DATA =  (0x00);
-        delay();
-    }
+
+void setup(void){// konfiguriert den MiKrocontroller
+
+    // konfiguriere System-Clock
+    SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
+    uint32_t period = SysCtlClockGet()/SAMPLERATE;
+
+    // aktiviere Peripherie
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+
+    // konfiguriere GPIO
+    GPIOPinTypeADC(GPIO_PORTE_BASE,GPIO_PIN_2);
+    GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE,GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7);
+
+    // konfiguriere Timer
+    TimerConfigure(TIMER0_BASE,TIMER_CFG_PERIODIC);
+    TimerLoadSet(TIMER0_BASE, TIMER_A, period - 1);
+    TimerControlTrigger(TIMER0_BASE,TIMER_A,true);
+    TimerEnable(TIMER0_BASE,TIMER_A);
+
+    // konfiguriere ADC
+    ADCClockConfigSet(ADC0_BASE,ADC_CLOCK_RATE_FULL,1);
+    ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_TIMER, 0);
+    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH1|ADC_CTL_IE|ADC_CTL_END);
+    ADCSequenceEnable(ADC0_BASE, 3);
+    ADCIntClear(ADC0_BASE,3);
+    ADCIntRegister(ADC0_BASE,3,adcIntHandler);
+    ADCIntEnable(ADC0_BASE,3);
+
+}
+
+void adcIntHandler (void){
+   uint32_t adcInputValue;
+   ADCSequenceDataGet(ADC0_BASE,3,&adcInputValue);
+   // Bitte Code hier einfuegen
+   // ...
+
+   // am Ende von adcIntHandler, Interrupt-Flag loeschen
+   ADCIntClear(ADC0_BASE,3);
 }
